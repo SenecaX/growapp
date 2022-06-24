762af26d1e798db94ee46f73f51bac73a1fba8a8
Kedaren <kedaren.dorasami@yocto.re>
2022-06-24 16:21:01 +0400

    msg

--- src/components/DiaryWidget.js
+++ src/components/DiaryWidget.js
@@ -27,7 +27,7 @@ function DiaryWidget(props) {
       <View style={styles.textContainer}>
         <Text style={styles.title}>{props.title}</Text>
         <Text style={styles.week}>
-          {props.week[props.week.length - 1].weekNum}
+          Week {props.week[props.week.length - 1].weekNum}
         </Text>
       </View>
     </Pressable>
