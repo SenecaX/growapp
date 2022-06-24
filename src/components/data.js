0dd62fe76af277759d72d6ae2c7c3f94c093f051
Kedaren <kedaren.dorasami@yocto.re>
2022-06-24 16:29:23 +0400

    msg

--- src/components/data.js
+++ src/components/data.js
@@ -1,18 +1,21 @@
 const data = [
   {
-    title: "Aenean leo",
-    body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
-    imgUrl: "https://picsum.photos/id/11/200/300",
+    title: "Lettuce",
+    body: "",
+    imgUrl:
+      "https://media.istockphoto.com/photos/lettuce-salad-isolated-on-white-picture-id463197251?k=20&m=463197251&s=612x612&w=0&h=arqVi5DvGj0-wI53JU0lSmOMDOJBa2E_5iJ4f24BTwk=",
   },
   {
-    title: "In turpis",
-    body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
-    imgUrl: "https://picsum.photos/id/10/200/300",
+    title: "Bell pepper",
+    body: "",
+    imgUrl:
+      "https://www.gardeningknowhow.com/wp-content/uploads/2008/08/pepper.jpg",
   },
   {
-    title: "Lorem Ipsum",
-    body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
-    imgUrl: "https://picsum.photos/id/12/200/300",
+    title: "Bell pepper",
+    body: "",
+    imgUrl:
+      "https://cdn2.vectorstock.com/i/1000x1000/24/26/set-bell-pepper-plant-with-roots-isolated-vector-34582426.jpg",
   },
 ];
 
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -40,12 +40,6 @@ function Diary(props) {
 
   let showBtns = props.route.params.showSubmitBtn === true ? true : false;
 
-  // if (typeof diary === "object" && !Array.isArray(diary) && diary !== null) {
-  //   roomType = diary.diaryInfo.roomType;
-  //   wateringType = diary.diaryInfo.wateringType;
-  //   mediumType = diary.diaryInfo.mediumType;
-  // }
-
   useEffect(() => {
     async function getDiariesByName() {
       const diaries = await getDiariesByKey();
@@ -58,62 +52,6 @@ function Diary(props) {
     const id = data.id;
     const res = putNewWeek(data.diary.id, data.diary);
   }
-
-  // const data = [
-  //   {
-  //     key: 1,
-  //     title: "Week 0",
-  //     weekType: "Ger",
-  //   },
-  //   {
-  //     key: 2,
-  //     title: "Week 1",
-  //     weekType: "Veg",
-  //   },
-  //   {
-  //     key: 3,
-  //     title: "Week 2",
-  //     weekType: "Veg",
-  //   },
-  //   {
-  //     key: 4,
-  //     title: "Week 3",
-  //     weekType: "Veg",
-  //   },
-  //   {
-  //     key: 5,
-  //     title: "Week 6",
-  //     weekType: "Veg",
-  //   },
-  //   {
-  //     key: 6,
-  //     title: "Week 6",
-  //     weekType: "Flo",
-  //   },
-  //   {
-  //     key: 7,
-  //     title: "Week 7",
-  //     weekType: "Flo",
-  //   },
-  // ];
-
-  const carouselItems = [
-    {
-      title: "Aenean leo",
-      body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
-      imgUrl: "https://picsum.photos/id/11/200/300",
-    },
-    {
-      title: "In turpis",
-      body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
-      imgUrl: "https://picsum.photos/id/10/200/300",
-    },
-    {
-      title: "Lorem Ipsum",
-      body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
-      imgUrl: "https://picsum.photos/id/12/200/300",
-    },
-  ];
 
   function _renderItem({ item, index }) {
     return (
