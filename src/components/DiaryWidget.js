2bf6761b802719850495cf37f034dff5ceb45cbf
Kedaren <kedaren.dorasami@yocto.re>
2022-06-24 16:11:45 +0400

    added image

--- src/components/DiaryWidget.js
+++ src/components/DiaryWidget.js
@@ -1,19 +1,34 @@
 import { StyleSheet, View, Text, Image, Pressable } from "react-native";
 
 function DiaryWidget(props) {
+  const src =
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-keep-ducks-call-ducks-1615457181.jpg?resize=640:*";
   return (
     <Pressable
       style={styles.titleContainer}
       onPress={props.goToDiary}
       key={props.title}
     >
+      {/* source={src}  */}
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={props.src} />
+        <Image
+          style={styles.image}
+          source={{
+            uri: src,
+            method: "POST",
+            headers: {
+              Pragma: "no-cache",
+            },
+            body: "Your Body goes here",
+          }}
+        />
       </View>
 
       <View style={styles.textContainer}>
         <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.week}>{props.week[props.week.length -1].weekNum}</Text>
+        <Text style={styles.week}>
+          {props.week[props.week.length - 1].weekNum}
+        </Text>
       </View>
     </Pressable>
   );
